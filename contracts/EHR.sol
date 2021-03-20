77f9c44b1d1a7bed4383a765bb2a7db3c467a3a2
Claesse <oscar@hakonsult.se>
2021-03-20 21:08:00 +0100

    3 new functions

--- contracts/EHR.sol
+++ contracts/EHR.sol
@@ -1,19 +1,20 @@
 pragma solidity ^0.5.2;
+pragma experimental ABIEncoderV2;
 
 contract EHR {
 
 	address owner;
-	uint public patientCount;
-	mapping(uint => Patient) public patients;
-	uint public doctorCount;
-	mapping(uint => Doctor) public doctors;
-	mapping (address => bool) profileExists;
+	//uint public patientCount;
+	mapping(address => Patient) public patients;
+	//uint public doctorCount;
+	mapping(address=> Doctor) public doctors;
+	mapping(address => bool) profileExists;
 
 	// Model a patient
 	struct Patient {
 
 		// Identifiers
-		uint id;
+		address id;
 		string name;
 		uint birthyear;
 		uint month;
@@ -23,23 +24,41 @@ contract EHR {
 		// Health record
 		string[] records;
 		// Doctor list
-		string[] doctorAccess;
+		address[] doctorAccess;
 
 	}
-	// Register
+	// Register patient
 	function registerPatient(string memory _name, uint _birthyear, uint _month, uint _day, string memory _city) public {
 		owner = msg.sender;
 		require(!profileExists[owner]);
-		patientCount++;
-		patients[patientCount] = Patient(patientCount, _name, _birthyear, _month, _day, _city);
+		//patientCount++;
+		patients[owner] = Patient(owner, _name, _birthyear, _month, _day, _city, new string[](0), new address[](0));
 		profileExists[owner] = true;
 
 	}
+	// Give doctor access to records.
+	// Need to solve require.
+	function giveAccess(address _doctor) public {
+		patients[msg.sender].doctorAccess.push(_doctor);
+	}
+	// Revoke doctor access to records.
+	// Need to solve require.
+	function revokeAccess(address _doctor) public {
+		for (uint i = 0; i<patients[msg.sender].doctorAccess.length; i++) {
+			if (patients[msg.sender].doctorAccess[i] == _doctor) {
+				delete patients[msg.sender].doctorAccess[i];
+			}
+		}
+    }
 
-	struct Doctor{
+    function viewRecord(address _patient) public view returns (string[] memory){
+    	return patients[_patient].records;
+    }
+
+	struct Doctor {
 
 	// Identifiers
-	uint id;
+	address id;
 	string name;
 	uint birthyear;
 	uint month;
@@ -52,12 +71,12 @@ contract EHR {
 	
 
 	}
-
+	// Register doctor
 	function registerDoctor(string memory _name, uint _birthyear, uint _month, uint _day, string memory _city, string memory _speciality) public {
 		owner = msg.sender;
 		require(!profileExists[owner]);
-		doctorCount++;
-		doctors[doctorCount] = Doctor(doctorCount, _name, _birthyear, _month, _day, _city, _speciality);
+		//doctorCount++;
+		doctors[owner] = Doctor(owner, _name, _birthyear, _month, _day, _city, _speciality, new string[](0));
 		profileExists[owner] = true;
 	}
 	
